e51b7ea5d9d58623a2841b585c5801184c31042a
polllandreuy <54383678+polllandre05@users.noreply.github.com>
2023-06-12 23:19:28 +0800

    Update: Track Order Implmentation

--- lib/Buyer/trackorder.dart
+++ lib/Buyer/trackorder.dart
@@ -3,40 +3,157 @@ import 'package:flutter/material.dart';
 import '../widgets.dart';
 
 class TrackOrder extends StatefulWidget {
-  final Map transaction;
-  const TrackOrder({Key? key, required this.transaction}) : super(key: key);
+  const TrackOrder({Key? key}) : super(key: key);
+
   @override
   _TrackOrderState createState() => _TrackOrderState();
 }
 
 class _TrackOrderState extends State<TrackOrder> {
+  Map<int, String> orderStatus = {
+    1: 'Order Placed',
+    2: 'Order Confirmed',
+    3: 'Order Processed',
+    4: 'Ready to Ship',
+    5: 'Out for Delivery',
+  };
+  String currentStatus = 'Order Confirmed';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppbar(context, 'Track Order', false),
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[],
-        ),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 111, 174, 23),
+        centerTitle: true,
+        title: Text('Track Order'),
       ),
-      bottomNavigationBar: ElevatedButton(
-        style: ButtonStyle(
-            backgroundColor:
-                MaterialStateProperty.all(Color.fromARGB(255, 111, 174, 23)),
-            padding:
-                MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 50)),
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ))),
-        onPressed: () {},
-        child: const Text(
-          'ORDER RECEIVED',
-          style: TextStyle(
-              fontSize: 14,
-              fontFamily: 'Montserrat',
-              letterSpacing: 2.2,
-              color: Colors.white),
+      body: Container(
+        color: Color.fromARGB(255, 236, 236, 163),
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Flexible(
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(
+                      255,
+                      178,
+                      218,
+                      121,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: SafeArea(
+                    child: ExpansionTile(
+                      leading: const Icon(Icons.list_sharp,
+                          color: Color.fromARGB(255, 111, 174, 23), size: 25.0),
+                      title: Text(
+                        "Order Status",
+                        style: const TextStyle(
+                            color: Colors.black,
+                            fontFamily: 'Visby',
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      children: <Widget>[
+                        ListView.builder(
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          itemCount: orderStatus.length,
+                          itemBuilder: (context, index) {
+                            int stepNumber = index + 1;
+                            String status = orderStatus[stepNumber]!;
+                            bool isCurrentStatus = status == currentStatus;
+                            bool isBeforeCurrentStatus = stepNumber <
+                                orderStatus.keys.firstWhere(
+                                  (key) => orderStatus[key] == currentStatus,
+                                  orElse: () => 1,
+                                );
+                            String stepStatus =
+                                isBeforeCurrentStatus ? 'Done' : 'Pending';
+                            Color stepNumberColor = isCurrentStatus
+                                ? Colors.white
+                                : (isBeforeCurrentStatus
+                                    ? Color.fromARGB(255, 111, 174, 23)
+                                    : Colors.grey);
+                            Color stepStatusColor = isCurrentStatus
+                                ? Colors.white
+                                : (isBeforeCurrentStatus
+                                    ? Color.fromARGB(255, 111, 174, 23)
+                                    : Colors.grey);
+
+                            return Column(
+                              children: [
+                                ListTile(
+                                  leading: Container(
+                                    width: 24,
+                                    height: 24,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: stepNumberColor,
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        stepNumber.toString(),
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: isCurrentStatus
+                                              ? Color.fromARGB(
+                                                  255, 111, 174, 23)
+                                              : Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  title: Text(
+                                    status,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: stepStatusColor,
+                                    ),
+                                  ),
+                                  subtitle: Text(
+                                    isCurrentStatus
+                                        ? 'Current Status'
+                                        : stepStatus,
+                                    style: TextStyle(
+                                      fontStyle: FontStyle.italic,
+                                      color: isCurrentStatus
+                                          ? Colors.white
+                                          : (isBeforeCurrentStatus
+                                              ? Color.fromARGB(
+                                                  255, 111, 174, 23)
+                                              : Colors.grey),
+                                    ),
+                                  ),
+                                ),
+                                if (index < orderStatus.length - 1)
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 27),
+                                    child: Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: FractionallySizedBox(
+                                        widthFactor: 0.005,
+                                        child: Container(
+                                          height: 20,
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                              ],
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
